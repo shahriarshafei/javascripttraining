656f0689f433cb564e29d6248d9d7d8752f7e801
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-02-26 16:34:38 +0330

    adding another exercise

--- script.js
+++ script.js
@@ -2,43 +2,23 @@
 // arrayName.push() -> add an item to the end of the array
 // var audio = new Audio("URL"); -> audio.play(); -> method for playing an audio file
 
-// Fibonnaci generator: "n" dedicates the amount of numbers inside the array. output is in form of array
-
 // Exrecise 1: Write a program that inputs user name and alrts it with the first charachter in uppercase form and the rest in lower case, no matter how the user input it
 var userInput = prompt("Please enter your name: ");
 var firstChar = userInput.slice(0,1).toUpperCase();
 var restChar = userInput.slice(1, userInput.length).toLowerCase();
 alert(firstChar+restChar);
 
-// Exrecise 2: Write a program that outputs Fibonacci numbers of the desired lenght
-function fibonacciGenerator(n) {
-
-    var fibArr=[];
-
-    if (n===1){
-        fibArr=[0];
+// Exercise 2: Write a program that inputs your age and shows how many days, weeks, months and years remains of your life assuming you will die at 90
+var userAge = prompt ("Please enter your age: ");
+var userLeftYears = 90 - userAge;
+var userLeftMonths = userLeftYears*12; //assuming there are 12 months in a year
+var userLeftWeeks = userLeftYears*52;  //assuming there are 52 weeks in a year
+var userLeftDays = userLeftYears*365;  //assuming there are 365 days in a year
+if(userLeftYears===1){
+    alert("You have "+userLeftYears+" year, "+userLeftMonths+" months, "+
+                    userLeftWeeks+" weeks and "+userLeftDays+" days left to live");
     }
-
-    else if (n===2) {
-        fibArr=[0,1];
+else {
+alert("You have "+userLeftYears+" years, "+userLeftMonths+" months, "+
+                    userLeftWeeks+" weeks and "+userLeftDays+" days left to live");
     }
-
-    else {
-        fibArr = [0,1];
-        for (var i = 3; i<n; i++){
-            fibArr.push(fibArr[fibArr.length - 2] + fibArr[fibArr.length -1]);
-        }
-    }
-
-    return fibArr;
-}
-
-console.log(fibonacciGenerator(10));
-
-// Exercise 3: Dice number generator
-function diceGenerator() {
-var diceNumber = Math.floor(Math.random()*6)+1;
-    return diceNumber;
-}
-
-console.log(diceGenerator());
