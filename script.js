83ca2081461e05e31649db646bbcf39bab2f8285
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-02-26 13:25:53 +0330

    adding exercise 1

--- script.js
+++ script.js
@@ -12,6 +12,13 @@
 
 // Fibonnaci generator: "n" dedicates the amount of numbers inside the array. output is in form of array
 
+// Exrecise 1: Write a program that inputs user name and alrts it with the first charachter in uppercase form and the rest in lower case, no matter how the user input it
+var userInput = prompt("Please enter your name: ");
+var firstChar = userInput.slice(0,1).toUpperCase();
+var restChar = userInput.slice(1, userInput.length).toLowerCase();
+alert(firstChar+restChar);
+
+// Exrecise 2: Write a program that outputs Fibonacci numbers of the desired lenght
 function fibonacciGenerator(n) {
 
     var fibArr=[];
@@ -36,8 +43,7 @@ function fibonacciGenerator(n) {
 
 console.log(fibonacciGenerator(10));
 
-// Dice number generator
-
+// Exercise 3: Dice number generator
 function diceGenerator() {
 var diceNumber = Math.floor(Math.random()*6)+1;
     return diceNumber;
